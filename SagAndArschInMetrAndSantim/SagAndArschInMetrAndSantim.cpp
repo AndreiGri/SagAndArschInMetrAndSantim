c07858f6e2ffcf859f0e1e13a72165fc4567e80d
Gridin_Andrei <andreigridin918@gmail.com>
2023-08-01 21:16:32 +0500

    Добавьте файлы проекта.

---
+++ SagAndArschInMetrAndSantim/SagAndArschInMetrAndSantim.cpp
@@ -0,0 +1,36 @@
+﻿#include <iostream>
+#include <cstdlib>
+
+using namespace std;
+
+int SagenAndArschinInMetr(int sag, int arsch) {
+    const double metrInSag = 2.16;
+    const double metrInArsch = 2.16 / 3;
+    int metr = (int)(sag*metrInSag + arsch*metrInArsch);
+    return metr;
+}
+
+int SagenAndArschinInSantimetr(int sag, int arsch) {
+    const double metrInSag = 2.16;
+    const double metrInArsch = 2.16 / 3;
+    int santimetr = ((int)(sag * metrInSag * 100) + (int)(arsch * metrInArsch * 100)) % 100;
+    return santimetr;
+
+}
+
+int main()
+{
+    //Изменение кодировки консоли:
+    system("chcp 1251 > nul");
+    int sagen;
+    int arschin;
+    cout << "Введите кол-во саженей: ";
+    cin >> sagen;
+    cout << "и аршин: ";
+    cin >> arschin;
+    cout << "В " << sagen << " саженях и " << arschin << " аршинах " << SagenAndArschinInMetr(sagen, arschin) 
+        << " метров и " << SagenAndArschinInSantimetr(sagen, arschin) << " сантиметров." << endl;
+        //Задержка консольного окна:
+    system("pause > nul");
+    return 0;
+}
